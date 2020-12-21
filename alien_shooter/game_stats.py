20483fb4c8e1ddbfe85f4acc432a75957d17ad4e
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2020-12-21 11:32:29 -0500

    Coded Added.
    Whole code for game Alien Invasion. Need to update with score management.

---
+++ alien_shooter/game_stats.py
@@ -0,0 +1,12 @@
+class GameStats():
+	"""Tracks statistics for Alien Invasion."""
+
+	def __init__(self, ai_settings):
+		"""Initializes statistics."""
+		self.ai_settings = ai_settings
+		self.reset_stats()
+		self.game_active = True
+
+	def reset_stats(self):
+		"""Initializes statistics that can change during the game."""
+		self.ships_left = self.ai_settings.ship_limit
